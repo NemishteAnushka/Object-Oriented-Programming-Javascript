ae06c100a411c1520cd75738203c6d8ce8cf143b
NemishteAnushka <nemishte.anushka23@gmail.com>
2025-01-28 23:31:18 +0530

    started prototype

---
+++ prototype/index.js
@@ -0,0 +1,12 @@
+//javascripts functions also works objects
+//for example
+function myFunc() {
+  console.log("hello");
+}
+console.log(myFunc.name); //this name property returns name of the function
+//also you can add your own properties
+//for eg
+myFunc.myName = "Anushka";
+console.log(myFunc.myName);
+
+//Also some usefull functions properties are call(),apply(),bind()
